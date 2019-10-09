20039890853a9ceb998be15b9e52be7ad989c4e4
Pedro Cisneros Santana <pedro.cisneros.santana@gmail.com>
2019-10-09 12:19:00 +0200

    Update Ejercicio08.java

--- cap05/Ejercicio08.java
+++ cap05/Ejercicio08.java
@@ -1,10 +1,22 @@
 package com.iescampanillas.ejercicios;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Ejercicio08 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s = new Scanner(System.in);
+		try {
+			System.out.print("Introduce un numero para hacer la tabla: ");
+			int num = s.nextInt();
+			int i;
+			for (i=0; i<=10; i++) {
+				System.out.printf("%d x %d = %d\n", i, num, i * num);
+			}
+			} catch (InputMismatchException e) {
+			System.out.println("Debes introducir un numero entero");
+			}
+		s.close();
+		}
+	
 	}
-
-}
