5353f5935c82a0281b4533010e7e44957cfd173d
Pedro Cisneros Santana <pedro.cisneros.santana@gmail.com>
2019-10-09 09:52:43 +0200

    Create Ejercicio05.java

---
+++ cap05/Ejercicio05.java
@@ -0,0 +1,14 @@
+package com.iescampanillas.ejercicios;
+
+public class Ejercicio05 {
+
+	public static void main(String[] args) {
+		int cont = 320;
+		while (cont >= 160) {
+			System.out.println(cont);
+			cont -= 20;
+		}
+
+	}
+
+}
