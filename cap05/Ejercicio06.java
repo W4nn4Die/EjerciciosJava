96a963d644bb2dc194a2356b2ef66ae7b6851817
Pedro Cisneros Santana <pedro.cisneros.santana@gmail.com>
2019-10-09 09:52:46 +0200

    Create Ejercicio06.java

---
+++ cap05/Ejercicio06.java
@@ -0,0 +1,14 @@
+package com.iescampanillas.ejercicios;
+
+public class Ejercicio06 {
+
+	public static void main(String[] args) {
+		int cont = 320;
+		do {
+			System.out.println(cont);
+			cont -= 20;
+		} while (cont >= 160);
+
+	}
+
+}
