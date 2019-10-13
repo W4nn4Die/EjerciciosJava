1545389d67bfa97d0f52b7443c330eb8c81c9f42
Pedro Cisneros Santana <pedro.cisneros.santana@gmail.com>
2019-10-13 23:20:00 +0200

    Create Ejercicio20.java

---
+++ cap05/Ejercicio20.java
@@ -0,0 +1,36 @@
+package com.iescampanillas.ejercicios;
+import java.util.Scanner;
+
+public class Ejercicio20 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.print("Introduce la altura de la piramide:");
+		int alt = s.nextInt();
+		System.out.print("Introduce el caracter para llenar la piramide: ");
+		String relleno = s.next();
+		int base = 1;
+		int espacios = alt - 1;
+		int vacio = 0;
+		while (base <= alt) {
+			for (int i = 1; i <= espacios; i++) {
+				System.out.print(" ");
+			}
+			System.out.print(relleno);
+			for (int i = 1; i <= vacio; i++) {
+				System.out.print(" ");
+			}
+			
+			if (alt > 1) {
+				System.out.print(relleno);
+			}
+			System.out.println();
+			
+			base++;
+			espacios--;
+			vacio += 2;
+		}
+		s.close();
+	}
+
+}
