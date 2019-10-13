8292e3e0dfdceeffb143156812ec3e4160c0aaf8
Pedro Cisneros Santana <pedro.cisneros.santana@gmail.com>
2019-10-13 23:19:51 +0200

    Create Ejercicio16.java

---
+++ cap05/Ejercicio16.java
@@ -0,0 +1,13 @@
+package com.iescampanillas.ejercicios;
+import java.util.Scanner;
+
+public class Ejercicio16 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Introduce un numero para ver si es primo: ");
+		int isPrimo = s.nextInt();
+		s.close();
+	}
+
+}
