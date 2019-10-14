63c4cce6e8021043a1b4339a3f100a2e55dc62fb
Pedro Cisneros Santana <pedro.cisneros.santana@gmail.com>
2019-10-14 18:17:17 +0200

    Create Ejercicio28.java

---
+++ cap05/Ejercicio28.java
@@ -0,0 +1,12 @@
+package com.iescampanillas.ejercicios;
+import java.util.Scanner;
+
+public class Ejercicio28 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		s.close();
+	}
+
+}
