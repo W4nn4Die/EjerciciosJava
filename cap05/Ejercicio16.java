3ec549550413d530e7dffe33503f3ca4bbc2abc4
Pedro Cisneros Santana <pedro.cisneros.santana@gmail.com>
2019-10-14 18:31:04 +0200

    Update Ejercicio16.java

--- cap05/Ejercicio16.java
+++ cap05/Ejercicio16.java
@@ -7,6 +7,19 @@ public class Ejercicio16 {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Introduce un numero para ver si es primo: ");
 		int isPrimo = s.nextInt();
+		int mod = 0;
+		int cont = 0;
+		for (int i = 1; i<=isPrimo; i++) {
+			mod = isPrimo % i;
+			if (mod == 0) {
+				cont += 1;
+			}
+		}
+		if (cont == 2) {
+			System.out.println(isPrimo + " es primo");
+		} else {
+			System.out.println(isPrimo + " no es primo");
+		}
 		s.close();
 	}
 
