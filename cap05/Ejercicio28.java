5c9750c7f864200af0a034a6776a700e47ce51c6
Pedro Cisneros Santana <pedro.cisneros.santana@gmail.com>
2019-10-14 22:08:51 +0200

    Update Ejercicio28.java

--- cap05/Ejercicio28.java
+++ cap05/Ejercicio28.java
@@ -5,7 +5,13 @@ public class Ejercicio28 {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		
+		System.out.print("Introduce un numero para calcular su factorial: ");
+		int num = s.nextInt();
+		double fact = 1;
+		for (int i = 1; i<=num; i++) {
+			fact *= i;
+		}
+		System.out.printf("El factorial de %d es: %.0f", num, fact);
 		s.close();
 	}
 
