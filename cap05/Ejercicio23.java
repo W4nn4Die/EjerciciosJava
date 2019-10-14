f8ff0afd83a486f1c1dd16c5aac1d6d04677db78
Pedro Cisneros Santana <pedro.cisneros.santana@gmail.com>
2019-10-14 13:05:43 +0200

    Update Ejercicio23.java

--- cap05/Ejercicio23.java
+++ cap05/Ejercicio23.java
@@ -3,7 +3,7 @@ package com.iescampanillas.ejercicios;
 public class Ejercicio23 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("");
 
 	}
 
