60913f41819c40aa5bf4be82af121c90fd4f950e
Pedro Cisneros Santana <pedro.cisneros.santana@gmail.com>
2019-10-16 10:47:44 +0200

    Update Ejercicio10.java

--- cap05/Ejercicio10.java
+++ cap05/Ejercicio10.java
@@ -2,14 +2,14 @@ package com.iescampanillas.ejercicios;
 import java.util.Scanner;
 
 public class Ejercicio10 {
-
+	
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int num = 0;
 		int suma = 0;
 		int num1 = 0;
 		int total = 0;
-		for (int i = 0;num>=0;i+=num) {
+		for (@SuppressWarnings("unused")int i = 0;num>=0;i+=num) {
 			num1 = suma;
 			total++;
 			System.out.print("Introduce un numero para sumar\npara terminar introduce un numero negativo: ");
@@ -19,6 +19,7 @@ public class Ejercicio10 {
 		total = total -1;
 		System.out.printf("La media de todos los numeros es: %d", num1 / total);
 		s.close();
+		
 	}
 
 }
