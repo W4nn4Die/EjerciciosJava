dd17cd6bf249b1a9ab03cdca5e494955727842ee
Pedro Cisneros Santana <pedro.cisneros.santana@gmail.com>
2019-10-17 10:50:12 +0200

    Update Ejercicio23.java

--- cap05/Ejercicio23.java
+++ cap05/Ejercicio23.java
@@ -1,10 +1,19 @@
 package com.iescampanillas.ejercicios;
+import java.util.Scanner;
 
 public class Ejercicio23 {
 
 	public static void main(String[] args) {
-		System.out.println("");
-
+	  Scanner s = new Scanner(System.in);
+	  int sum = 0;
+	  while (sum <= 1000) {
+		System.out.print("Introduce un numero para sumar hasta llegar a 1000: ");
+		int num = s.nextInt();
+		sum += num;
+		System.out.println("Total: " + sum);
+	  }
+	  System.out.println("Has pasado 1000");
+	  s.close();
 	}
 
 }
