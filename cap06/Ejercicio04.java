be22561e5fcf16705b49060d868ba97b31419a26
Pedro Cisneros Santana <pedro.cisneros.santana@gmail.com>
2019-10-28 10:45:25 +0100

    Create Ejercicio04.java

---
+++ cap06/Ejercicio04.java
@@ -0,0 +1,12 @@
+package com.iescampanillas.ejercicios;
+
+public class Ejercicio04 {
+
+  public static void main(String[] args) {
+	for (int i = 1; i<=20; i++) {
+	  System.out.print((int) (Math.random()*10 + 1) + " ");
+	}
+
+  }
+
+}
