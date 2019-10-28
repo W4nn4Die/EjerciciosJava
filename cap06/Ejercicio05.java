bc63f1f715f69d98952cd95d3070ddecbcd44c33
Pedro Cisneros Santana <pedro.cisneros.santana@gmail.com>
2019-10-28 10:54:04 +0100

    Update Ejercicio05.java

--- cap06/Ejercicio05.java
+++ cap06/Ejercicio05.java
@@ -3,8 +3,24 @@ package com.iescampanillas.ejercicios;
 public class Ejercicio05 {
 
   public static void main(String[] args) {
-	System.out.println((int)(Math.random()*99 + 100));
-
+	int rand = 0;
+	int max = 0;
+	int min = 200;
+	int total = 0;
+	for (int i = 1; i<= 50; i++) {
+	  rand = (int)(Math.random()*99 + 100);
+	  if (rand > max) {
+		max = rand;
+	  } 
+	  if (rand < min) {
+		min = rand;
+	  }
+	  total += rand;
+	  System.out.println(rand);
+	}
+	int media = total / 50;
+	System.out.printf("Maximo: %d\nMinimo: %d\n"
+		+ "Media: %d", max, min, media);
   }
 
 }
