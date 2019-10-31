4d9cf0229443267791b5add710c4e7061e1ce6ee
Pedro Cisneros Santana <pedro.cisneros.santana@gmail.com>
2019-10-31 09:27:12 +0100

    Create Ejercicio09.java

---
+++ cap06/Ejercicio09.java
@@ -0,0 +1,14 @@
+package com.iescampanillas.ejercicios;
+
+public class Ejercicio09 {
+
+  public static void main(String[] args) {
+	int valor = 0;
+	while (valor != 24) {
+	  valor = (int) (Math.random()*(100 - 0) + 1) + 0;
+	  System.out.printf("%d\n", valor); 
+	}
+
+  }
+
+}
