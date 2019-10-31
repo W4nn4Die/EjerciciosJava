756027ef7f2d56985427411bf3631bf7ae71859c
Pedro Cisneros Santana <pedro.cisneros.santana@gmail.com>
2019-10-31 09:07:37 +0100

    Update Ejercicio07.java

--- cap06/Ejercicio07.java
+++ cap06/Ejercicio07.java
@@ -3,7 +3,24 @@ package com.iescampanillas.ejercicios;
 public class Ejercicio07 {
 
   public static void main(String[] args) {
-	// TODO Auto-generated method stub
+	int valor = 0;
+	String strValor = "";
+	int cont = 1;
+	while (cont <= 14) {
+	  for (int x = 1; x<= 3; x++) {
+		valor = (int) (Math.random()*3 + 1);
+		  if (valor == 1) {
+			strValor += "|1  ";
+		  } else if (valor == 2) {
+			strValor += "| X ";
+		  } else if (valor == 3){
+			strValor += "|  2";
+		  }
+	  }
+	  System.out.printf("%-3d%s|\n", cont, strValor);
+	  strValor = "";
+	  cont++;
+	}
 
   }
 
