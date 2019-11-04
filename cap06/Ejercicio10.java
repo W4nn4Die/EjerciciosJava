92e642003aed55b0a2f9c1cdf8beee7f702948da
Pedro Cisneros Santana <pedro.cisneros.santana@gmail.com>
2019-11-04 09:57:48 +0100

    Update Ejercicio10.java

--- cap06/Ejercicio10.java
+++ cap06/Ejercicio10.java
@@ -2,6 +2,7 @@ package com.iescampanillas.ejercicios;
 
 public class Ejercicio10 {
   
+  public static void getCar() {
 	int rand = (int) (Math.random()*40 + 1);
 	int randCar = (int) (Math.random()*6 + 1);
 	String tempCar = "";
