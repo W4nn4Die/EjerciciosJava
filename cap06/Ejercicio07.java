ea907dce15a7378858c95db44e7c05432952b89b
Pedro Cisneros Santana <pedro.cisneros.santana@gmail.com>
2019-11-04 08:42:40 +0100

    Update Ejercicio07.java

--- cap06/Ejercicio07.java
+++ cap06/Ejercicio07.java
@@ -1,26 +1,34 @@
 package com.iescampanillas.ejercicios;
 
 public class Ejercicio07 {
-
+  public static String strValor = "";
+  public static int valor = 0;
   public static void main(String[] args) {
-	int valor = 0;
-	String strValor = "";
 	int cont = 1;
 	while (cont <= 14) {
 	  for (int x = 1; x<= 3; x++) {
 		valor = (int) (Math.random()*3 + 1);
-		  if (valor == 1) {
-			strValor += "|1  ";
-		  } else if (valor == 2) {
-			strValor += "| X ";
-		  } else if (valor == 3){
-			strValor += "|  2";
-		  }
+		if (valor == 1) {
+		  strValor += "|1  ";
+		} else if (valor == 2) {
+		  strValor += "| X ";
+		} else if (valor == 3){
+		  strValor += "|  2";
+		}
 	  }
 	  System.out.printf("%-3d%s|\n", cont, strValor);
 	  strValor = "";
 	  cont++;
 	}
+	valor = (int) (Math.random()*3 + 1);
+	if (valor == 1) {
+	  strValor += "|1  |";
+	} else if (valor == 2) {
+	  strValor += "| X |";
+	} else if (valor == 3){
+	  strValor += "|  2|";
+	}
+	System.out.printf("%-3d%s", 15, strValor);
 
   }
 
