2f5cb0564503a3084acd1fb41fe1385654f9a624
Pedro Cisneros Santana <pedro.cisneros.santana@gmail.com>
2019-11-04 09:14:48 +0100

    Update Ejercicio09.java

--- cap06/Ejercicio09.java
+++ cap06/Ejercicio09.java
@@ -5,7 +5,7 @@ public class Ejercicio09 {
   public static void main(String[] args) {
 	int valor = 0;
 	while (valor != 24) {
-	  valor = (int) (Math.random()*(100 - 0) + 1) + 0;
+	  valor = (int) (Math.random()*100 + 1);
 	  System.out.printf("%d\n", valor);
 	  cont++;
 	}
