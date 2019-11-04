ae7b4e774271e15ad43e82ea5bc05d36dd948bab
Pedro Cisneros Santana <pedro.cisneros.santana@gmail.com>
2019-11-04 08:46:20 +0100

    Update Ejercicio09.java

--- cap06/Ejercicio09.java
+++ cap06/Ejercicio09.java
@@ -1,13 +1,15 @@
 package com.iescampanillas.ejercicios;
 
 public class Ejercicio09 {
-
+  public static int cont = 0;
   public static void main(String[] args) {
 	int valor = 0;
 	while (valor != 24) {
 	  valor = (int) (Math.random()*(100 - 0) + 1) + 0;
-	  System.out.printf("%d\n", valor); 
+	  System.out.printf("%d\n", valor);
+	  cont++;
 	}
+	System.out.println("Total de números generados: " + cont);
 
   }
 
