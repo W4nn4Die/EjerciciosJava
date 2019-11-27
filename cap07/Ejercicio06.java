ffd1153441d20547aceddd556c32d2435625ac8d
Pedro Cisneros Santana <pedro.cisneros.santana@gmail.com>
2019-11-27 11:06:01 +0100

    Update Ejercicio06.java

--- cap07/Ejercicio06.java
+++ cap07/Ejercicio06.java
@@ -7,26 +7,24 @@ public class Ejercicio06 {
   public static void main(String[] args) {
 	Scanner s = new Scanner(System.in);
 	int[] num = new int[15];
-	//int[] num = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
-	int[] copy = num;
 	int restantes = 0;
 	while (restantes <= 14) {
 	  System.out.print("Introduce un numero: ");
 	  num[restantes] = s.nextInt();
 	  restantes++;
 	}
+	System.out.print("Old array: ");
 	for (int i: num) {
 	  System.out.print(i + " ");
 	}
 	restantes = 14;
-	//num[0] = copy[14];
-	//num[0] = copy[14];
 	int temp = num[14];
 	for (int i = (restantes - 1); i>= 0; i--) {
 	  num[i+1] = num[i];
 	}
 	num[0] = temp;
 	System.out.println("");
+	System.out.print("New array: ");
 	for (int i: num) {
 	  System.out.print(i + " ");
 	}
