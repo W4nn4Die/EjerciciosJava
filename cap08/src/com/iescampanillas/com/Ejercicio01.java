496d78324755d5430835f3179228514eb9c7655f
Pedro Cisneros Santana <pedro.cisneros.santana@gmail.com>
2019-12-03 08:54:42 +0100

    Update Ejercicio01.java

--- cap08/src/com/iescampanillas/com/Ejercicio01.java
+++ cap08/src/com/iescampanillas/com/Ejercicio01.java
@@ -1,7 +1,7 @@
 package com.iescampanillas.com;
 
 public class Ejercicio01 {
-  boolean esCapicua(long num) {
+  public static boolean esCapicua(long num) {
 	long numero = num;
 	long volt = 0;
 	while (numero > 0) {
@@ -15,12 +15,53 @@ public class Ejercicio01 {
 	}
   }
   
-  boolean esPrimo(long num) {
+  public static boolean esPrimo(long num) {
 	if ((num % 2) == 0) {
 	  return true;
 	} else {
 	  return false;
 	}
+  }
+  
+  public static long siguientePrimo(long num) {
+	while (!esPrimo(num++)) {
+	  
+	}
+	return num;
+  }
+  
+  public static double potencia(int base, int exponente) {
+	if (exponente == 0) {
+	  return 1;
+	}
+
+	if (exponente < 0) {
+	  return 1/potencia(base, -exponente);
+	}
+
+	int n = 1;
+
+	for (int i = 0; i < Math.abs(exponente); i++) {
+	  n = n * base;
+	}
+
+	return n;
+  }
+  
+  public static int digitos(long num) {
+	int dig = 0;
+	if (num < 0) {
+	  num = -num;
+	}
+	if (num == 0) {
+	  return 1;
+	} else {
+	  while (num > 0) {
+		num /= 10;
+		dig++;
+	  }
+	}
+	return dig;
   }
 
   public static void main(String[] args) {
